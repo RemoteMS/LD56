8b7984fbb22a2981b9dec58e49ba1c2f627591e0
RemoteMS <rostov.90.100@gmail.com>
2024-10-07 20:53:21 +0400

    18-72

--- Assets/Scripts/Common/Enemy/EnemyRunner.cs
+++ Assets/Scripts/Common/Enemy/EnemyRunner.cs
@@ -6,18 +6,45 @@ namespace Common.Enemy
     [RequireComponent(typeof(NavMeshAgent))]
     public class EnemyRunner : MonoBehaviour
     {
+        private static readonly int Walk = Animator.StringToHash("Walk");
         [SerializeField] private Transform Player;
+        [SerializeField] private Animator animator;
         [SerializeField] private NavMeshAgent navMeshAgent;
-        [SerializeField] private float timeInterval = 5f;
+        [SerializeField] private float updateInterval = 5f;
 
-        private void Start()
+        [SerializeField] private float stoppingDistance = 1f;
+
+        private float _timeSinceLastUpdate = 0f;
+
+        private void Update()
         {
-            InvokeRepeating(nameof(MoveTowardsPlayer), 0f, timeInterval);
+            _timeSinceLastUpdate += Time.deltaTime;
+
+            if (_timeSinceLastUpdate >= updateInterval)
+            {
+                MoveTowardsPlayer();
+                _timeSinceLastUpdate = 0f;
+            }
+
+            CheckIfReachedDestination();
         }
 
         private void MoveTowardsPlayer()
         {
+            animator.SetBool(Walk, true);
             navMeshAgent.SetDestination(Player.position);
         }
+
+        private void CheckIfReachedDestination()
+        {
+            if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance <= stoppingDistance)
+            {
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
+                {
+                    animator.SetBool(Walk, false);
+                }
+            }
+        }
     }
+
 }
